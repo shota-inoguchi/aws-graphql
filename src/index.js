0e9b5de2b3a232932ba52171ed5435968542e173
unknown <230450094@kccs.auth>
2023-08-29 09:05:26 +0900

    gql

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import App from './App';
+
+const client = new ApolloClient({
+  uri: 'YOUR_GRAPHQL_API_ENDPOINT', // 例: 'https://api.example.com/graphql'
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
